6b7a801ad385c474b128ac4c3c48a493a5a07f21
sebins <150359972+Aonickname@users.noreply.github.com>
2024-08-27 23:44:04 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]){
+
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+
+    return 0;
+}
