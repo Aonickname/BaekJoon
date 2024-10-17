d50e0144b2aa28fef75afc8223e50913788a4800
sebins <150359972+Aonickname@users.noreply.github.com>
2024-10-17 12:42:47 +0900

    [Bronze II] Title: 상수, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Bronze/2908. 상수/상수.py
+++ Python/백준/Bronze/2908. 상수/상수.py
@@ -1,21 +1,8 @@
-A, B = map(int, input().split())
+num1, num2 = input().split()
+num1 = int(num1[::-1])
+num2 = int(num2[::-1])
 
-revA = []
-revB = []
-
-for i in range(3):
-    revA.append(A%10)
-    A //= 10
-    revB.append(B%10)
-    B //= 10
-A = 0
-B = 0
-
-for i in range(3):
-    A += revA[2-i] * 10**i
-    B += revB[2-i] * 10**i
-
-if A > B:
-    print(A)
+if num1 > num2:
+    print(num1)
 else:
-    print(B)
+    print(num2)
