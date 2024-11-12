97696ff6e27daf32571f1f7ec00823ef26079a64
sebins <150359972+Aonickname@users.noreply.github.com>
2024-11-12 21:26:16 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,14 @@
+canvas = [[0] * 100 for _ in range(100)]
+
+N = int(input())
+
+for _ in range(N):
+    x, y = map(int, input().split())
+
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
+            canvas[i][j] = 1
+
+area = sum(sum(row) for row in canvas)
+
+print(area)
