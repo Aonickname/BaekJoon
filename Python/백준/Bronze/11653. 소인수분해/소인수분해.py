d768ad11d4ce5ff6d6d26a6eb2729b650da3bfc7
sebins <150359972+Aonickname@users.noreply.github.com>
2025-03-14 15:45:38 +0900

    [Bronze I] Title: 소인수분해, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/11653. 소인수분해/소인수분해.py
+++ Python/백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,17 +1,13 @@
 N = int(input())
-
-i = 2
-
 if N == 1:
     exit()
 
-while True:
-
-    if N % i == 0:
-        N = N // i
+i = 2
+while i*i <= N:
+    while N % i == 0:
         print(i)
-    else:
-        i += 1
-    
-    if N == 1:
-        break
+        N = N // i
+    i += 1
+
+if N > 1:
+    print(N)
