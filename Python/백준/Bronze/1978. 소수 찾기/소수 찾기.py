027a331357f917ef8ddfa4d65d3a320d92615f84
sebins <150359972+Aonickname@users.noreply.github.com>
2025-03-14 14:46:24 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ Python/백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -3,14 +3,28 @@ prime_nums = list(map(int, input().split()))
 
 result = [] 
 
-for i in range(N):
-    cnt = 0
+# for i in range(N):
+#     cnt = 0
 
-    for j in range(1, int(prime_nums[i]) + 1):
-        if prime_nums[i] % j == 0:
-            cnt += 1
+#     for j in range(1, int(prime_nums[i]) + 1):
+#         if prime_nums[i] % j == 0:
+#             cnt += 1
     
-    if cnt == 2:
-        result.append(prime_nums[i])
+#     if cnt == 2:
+#         result.append(prime_nums[i])
+
+for i in range(N):
+    num = prime_nums[i]
+    if num < 2:
+        continue
+
+    is_prime = True
+    for j in range(2, int(num**0.5) + 1):
+        if num % j == 0:
+            is_prime = False
+            break
+    
+    if is_prime:
+        result.append(num)
 
 print(len(result))
