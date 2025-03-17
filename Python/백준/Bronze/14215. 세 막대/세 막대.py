56d73f38abb71f40bf52ae6790de984ca5a4b7b2
sebins <150359972+Aonickname@users.noreply.github.com>
2025-03-17 19:16:36 +0900

    [Bronze III] Title: 세 막대, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/14215. 세 막대/세 막대.py
+++ Python/백준/Bronze/14215. 세 막대/세 막대.py
@@ -1,15 +1,9 @@
 a, b, c = map(int, input().split())
 
-nums = [a, b ,c]
-nums.sort()
+nums = sorted([a, b ,c])
 
-max_num = max(a, b, c)
-other_num_sum = nums[0] + nums[1]
-
-while True:
-
-    if max_num < other_num_sum:
-        print(max_num + other_num_sum)
-        break
-    else:
-        max_num -= 1
+if nums[2] < nums[0] + nums[1]:
+    print(sum(nums))
+else:
+    print(2 * (nums[0] + nums[1]) -1)
+    
