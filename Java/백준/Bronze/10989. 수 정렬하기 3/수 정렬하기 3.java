be61698f481ea9378c30ac54eee0d53e628594d0
sebins <150359972+Aonickname@users.noreply.github.com>
2025-08-03 21:20:25 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1536 ms, Memory: 336996 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] count = new int[10001];
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            count[num]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < count.length; i++) {
+            while (count[i]-- > 0) {
+                sb.append(i).append('\n');
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
