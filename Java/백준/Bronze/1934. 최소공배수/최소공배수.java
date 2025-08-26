ad7200aa13bb8f4a4f820901baa7ba7249a4e779
sebins <150359972+Aonickname@users.noreply.github.com>
2025-08-26 13:14:46 +0900

    [Bronze I] Title: 최소공배수, Time: 280 ms, Memory: 21716 KB -BaekjoonHub

--- Java/백준/Bronze/1934. 최소공배수/최소공배수.java
+++ Java/백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,6 +1,15 @@
 import java.util.*;
 
 public class Main {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -9,27 +18,10 @@ public class Main {
         for (int i = 0; i < T; i++) {
             int A = sc.nextInt();
             int B = sc.nextInt();
-            int remain = 0;
-            int GCD = 0;
-            int a, b;
 
-            if(A > B){
-                a = A;
-                b = B;
-            }else{
-                a = B;
-                b = A;
-            }
+            int GCD = gcd(A, B);
 
-            while(b != 0){
-                remain = a % b;
-                a = b;
-                b = remain;
-            }
-            GCD = a;
-
-            System.out.println((A*B) / GCD);
+            System.out.println((long)A * B / GCD);
         }
-
     }
 }
